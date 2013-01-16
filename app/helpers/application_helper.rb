766ec40321ae7d6e0687340d8343008eedade935
Eric Andres <emandres@gmail.com>
2013-01-16 10:32:25 -0700

    candidates page working better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def ballot_options_for_select
+    options_for_select Ballot.all.map { |b| [ b.name, b.id ] }
+  end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,5 @@
 class Candidate < ActiveRecord::Base
   belongs_to :ballot
-  attr_accessible :name
+  attr_accessible :name, :ballot_id
+  validates_presence_of :name, :ballot
 end
